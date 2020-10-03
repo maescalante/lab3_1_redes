7b35ee1b15dde398883b6a6fa62ece96f491410f
jccastiblancor <jc.castiblancor@uniandes.edu.co>
2020-10-03 13:12:38 -0500

    server

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,86 +1,58 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Server {
 
     int port;
     ArrayList<Socket> sockets;
+    ArrayList<ServerThread> conexiones;
 
-    //ServerSocket serverSocket = new ServerSocket(6868);
-
-    // Create a class constructor for the MyClass class
-    public Server() throws IOException {
+    public Server() {
         port = 5000;
-        sockets = new ArrayList<Socket>();
+        sockets = new ArrayList<>();
+        conexiones = new ArrayList<>();
     }
 
-    public void sendFile() {
+    public void main(){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        Server servidor = new Server(); // Create an object of class MyClass (This will call the constructor)
-
-        try (ServerSocket serverSocket = new ServerSocket(servidor.port)) {
-
-            System.out.println("Server is listening on port " + servidor.port);
+            System.out.println("Server is listening on port " + port);
 
             while (true) {
-                Socket socket = serverSocket.accept();
 
+                Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
 
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                // Agrega la conexion a la lista de conexiones activas
+                sockets.add(socket);
 
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
+                ServerThread thread = new ServerThread(socket);
+                thread.start();
+                conexiones.add(thread);
 
-                writer.println("Hello");
+                sendFile();
 
-                String text;
-
-                do {
-                    text = reader.readLine();
-
-                    String read = new StringBuilder(text).toString();
-
-                    //System.out.println(read);
-
-                    if (read.equals("Ready")){
-                        // Agrega la conexion a la lista de conexiones activas
-                        servidor.sockets.add(socket);
-
-                        // Envia el nombre del archivo que se va a mandar
-                        writer.println("prueba.txt");
-
-                        File file = new File("./prueba.txt");
-                        // Get the size of the file
-                        long length = file.length();
-                        byte[] bytes = new byte[16 * 1024];
-                        InputStream in = new FileInputStream(file);
-
-                        int count;
-                        while ((count = in.read(bytes)) > 0) {
-                            output.write(bytes, 0, count);
-                        }
-                        System.out.println("Finalizo el envio del archivo");
-                    }
-
-
-                } while (!text.equals("bye"));
-
-                writer.println("Se cerró la conexión");
-                socket.close();
             }
 
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
             ex.printStackTrace();
         }
+    }
+
+    public void sendFile() throws IOException {
+        File file = new File("./prueba.txt");
+        for(ServerThread conexion : conexiones)
+        {
+            System.out.println("enviando");
+            conexion.sendFile(file);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server servidor = new Server();
+        servidor.main();
     }
 
 }
---
+++ server/src/ServerThread.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * This thread is responsible to handle client connection.
+ *
+ */
+public class ServerThread extends Thread {
+    private Socket socket;
+    private InputStream input;
+    private BufferedReader reader;
+    private OutputStream output;
+    private PrintWriter writer;
+    private boolean isReady;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+        this.isReady = false;
+    }
+
+    public void run() {
+
+        try {
+            input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+
+            output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+
+            writer.println("Hello");
+
+            String text;
+
+            do {
+                text = reader.readLine();
+                String read = new StringBuilder(text).toString();
+                //System.out.println(read);
+                if (read.equals("Ready")){
+                    isReady = true;
+                    writer.println("Confirmado el estado para recibir archivos");
+                }
+            } while (!text.equals("bye"));
+
+            writer.println("Se cerró la conexión");
+            socket.close();
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendFile(File file) throws IOException {
+        // Envia el nombre del archivo que se va a mandar
+        writer.println("prueba.txt");
+
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            output.write(bytes, 0, count);
+        }
+        System.out.println("Finalizo el envio del archivo");
+    }
+}
