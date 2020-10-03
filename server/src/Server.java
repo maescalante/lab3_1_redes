c8685c5a371be39f2a0fccbd5dfda24d23154fbc
jccastiblancor <jc.castiblancor@uniandes.edu.co>
2020-10-03 12:06:27 -0500

    puuuuusshhhh

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,16 +1,19 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Server {
 
     int port;
+    ArrayList<Socket> sockets;
 
     //ServerSocket serverSocket = new ServerSocket(6868);
 
     // Create a class constructor for the MyClass class
     public Server() throws IOException {
         port = 5000;
+        sockets = new ArrayList<Socket>();
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,9 +41,30 @@ public class Server {
 
                 do {
                     text = reader.readLine();
-                    String reverseText = new StringBuilder(text).reverse().toString();
-                    System.out.println(reverseText);
-                    writer.println("Server: " + reverseText);
+
+                    String read = new StringBuilder(text).toString();
+
+                    //System.out.println(read);
+
+                    if (read.equals("Ready")){
+                        // Agrega la conexion a la lista de conexiones activas
+                        servidor.sockets.add(socket);
+
+                        // Envia el nombre del archivo que se va a mandar
+                        writer.println("prueba.txt");
+
+                        File file = new File("./prueba.txt");
+                        // Get the size of the file
+                        long length = file.length();
+                        byte[] bytes = new byte[16 * 1024];
+                        InputStream in = new FileInputStream(file);
+
+                        int count;
+                        while ((count = in.read(bytes)) > 0) {
+                            output.write(bytes, 0, count);
+                        }
+                    }
+
 
                 } while (!text.equals("bye"));
 
