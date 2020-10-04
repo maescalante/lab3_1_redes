eefe37d405d262d6a6060b705156388460d19822
maescalante <ma.escalante@uniandes.edu.co>
2020-10-04 10:10:12 -0500

    Jnb

--- server/src/ServerThread.java
+++ server/src/ServerThread.java
@@ -5,7 +5,6 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * This thread is responsible to handle client connection.
- *
  */
 public class ServerThread extends Thread {
     private Socket socket;
@@ -15,12 +14,13 @@ public class ServerThread extends Thread {
     private PrintWriter writer;
     private boolean isReady;
     private int index;
-    private boolean ok;
+
+
     public ServerThread(Socket socket, int index) {
         this.socket = socket;
         this.isReady = false;
         this.index = index;
-        this.ok=false;
+
     }
 
     public void run() {
@@ -39,12 +39,10 @@ public class ServerThread extends Thread {
             do {
                 text = reader.readLine();
                 String read = new StringBuilder(text).toString();
-                if (read.equals("Ready")){
+                if (read.equals("Ready")) {
                     isReady = true;
                 }
-                if (read.equals(("OK"))){
-                    ok=true;
-                }
+
             } while (!text.equals("bye"));
 
             writer.println("Closed connection");
@@ -62,49 +60,48 @@ public class ServerThread extends Thread {
         if (isReady) {
             // Envia el nombre del archivo que se va a mandar
             writer.println(file.getName());
+
+
+            //writer.println(file.length());
+
+            // Get the size of the file
+            //long length = file.length();
+            byte[] bytes = new byte[16 * 1024];
+            InputStream in = new FileInputStream(file);
+
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                output.write(bytes, 0, count);
+            }
+            System.out.println("Finalizo el envio del archivo");
+            output.close();
+            output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
             MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
             String shaChecksum = getFileChecksum(shaDigest, file);
             System.out.println(shaChecksum);
             writer.println(shaChecksum);
-            if (ok) {
-                //writer.println(file.length());
-
-                // Get the size of the file
-                //long length = file.length();
-                byte[] bytes = new byte[16 * 1024];
-                InputStream in = new FileInputStream(file);
-
-                int count;
-                while ((count = in.read(bytes)) > 0) {
-                    output.write(bytes, 0, count);
-                }
-                System.out.println("Finalizo el envio del archivo");
-                output.close();
-
-            }
-
 
 
         }
 
     }
 
-    private static String getFileChecksum(MessageDigest digest, File file) throws IOException
-    {
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
         FileInputStream fis = new FileInputStream(file);
         byte[] byteArray = new byte[1024];
         int bytesCount = 0;
 
         while ((bytesCount = fis.read(byteArray)) != -1) {
             digest.update(byteArray, 0, bytesCount);
-        };
+        }
+        ;
 
         fis.close();
         byte[] bytes = digest.digest();
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i< bytes.length ;i++)
-        {
+        for (int i = 0; i < bytes.length; i++) {
             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
         }
 
