d27a4dbb050ec26ddf589319a95591a77b65cd8d
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2020-10-03 20:46:26 -0500

    woooooooo

--- server/src/ServerThread.java
+++ server/src/ServerThread.java
@@ -69,51 +69,36 @@ public class ServerThread extends Thread {
             while ((count = in.read(bytes)) > 0) {
                 output.write(bytes, 0, count);
             }
-
-            //Use SHA-1 algorithm
-            MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
-
-            //SHA-1 checksum
-            String shaChecksum = getFileChecksum(shaDigest, file);
-
-            System.out.println(shaChecksum);
-            writer.println(shaChecksum);
-
             System.out.println("Finalizo el envio del archivo");
             output.close();
+
+            MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
+            String shaChecksum = getFileChecksum(shaDigest, file);
+            System.out.println(shaChecksum);
+            writer.println(shaChecksum);
         }
 
     }
 
     private static String getFileChecksum(MessageDigest digest, File file) throws IOException
     {
-        //Get file input stream for reading the file content
         FileInputStream fis = new FileInputStream(file);
-
-        //Create byte array to read data in chunks
         byte[] byteArray = new byte[1024];
         int bytesCount = 0;
 
-        //Read file data and update in message digest
         while ((bytesCount = fis.read(byteArray)) != -1) {
             digest.update(byteArray, 0, bytesCount);
         };
 
-        //close the stream; We don't need it now.
         fis.close();
-
-        //Get the hash's bytes
         byte[] bytes = digest.digest();
 
-        //This bytes[] has bytes in decimal format;
-        //Convert it to hexadecimal format
         StringBuilder sb = new StringBuilder();
         for(int i=0; i< bytes.length ;i++)
         {
             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
         }
 
-        //return complete hash
         return sb.toString();
     }
 }
